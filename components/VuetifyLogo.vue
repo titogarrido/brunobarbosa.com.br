8d0641e4801d380fdc0c9bd122e238f7060d5347
Tito Garrido <titogarrido@gmail.com>
2020-09-21 23:33:47 -0300

    first drop

---
+++ components/VuetifyLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-img src="/logo-horizontal.png"></v-img>
+</template>
+
+<style>
+.VuetifyLogo {
+  height: auto;
+  width: auto;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app light>
+    <v-app-bar height="100px" hide-on-scroll app flat color="transparent">
+      <img class="ml-5" src="/logo-horizontal.png" height="60" />
+      <v-spacer />
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-img height="600px" src="/banner2.jpg"></v-img>
+    <v-main class="mt-0 pt-0">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<style scoped>
+element.style {
+  background-position: top center;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      slides: [
+        {
+          src: "/banner2.jpg"
+        },
+        //{
+        //  src:
+        //    "http://localhost/unsafe/trim/smart/192.168.1.120:3000/banner3.jpg"
+        //}
+      ],
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Home",
+          to: "/"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Sobre",
+          to: "/inspire"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
